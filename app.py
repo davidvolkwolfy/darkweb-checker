c0ac70bfeb1b948364c4b71aad56944d72c86ee1
David Volk Wolfy <imcyberwolfy@gmail.com>
2025-05-07 02:36:56 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    results = None
+    error = None
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        if email:
+            headers = {
+                "User-Agent": "DarkWebCheckerApp"
+            }
+            url = f"https://haveibeenpwned.com/api/v3/breachedaccount/{email}"
+            try:
+                res = requests.get(url, headers=headers)
+                if res.status_code == 200:
+                    results = res.json()
+                elif res.status_code == 404:
+                    results = []  # No breach
+                else:
+                    error = f"Napaka pri iskanju: {res.status_code}"
+            except Exception as e:
+                error = f"Napaka: {str(e)}"
+
+    return render_template('index.html', results=results, error=error)
+
+if __name__ == '__main__':
+    app.run(debug=True)
